9dbee1becc0e3e14abfe61ef76f35f8f0b57bc82
Aaron Torrescano <atorrescano@gmail.com>
2018-08-29 00:03:56 -0500

    Added menu items, about and exit

--- Connect4Java/src/com/connect4/java/Connect4Java.java
+++ Connect4Java/src/com/connect4/java/Connect4Java.java
@@ -5,9 +5,19 @@
  */
 package com.connect4.java;
 
+import com.sun.glass.events.KeyEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JRootPane;
+import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
 
 /**
  *
@@ -31,12 +41,43 @@ public class Connect4Java {
         mainMenu = new JMenuBar();
         
         JMenu game = new JMenu("Game");
-        JMenu aboutConnect4 = new JMenu("About Connect4");
+        game.setMnemonic(KeyEvent.VK_G);
+        
+        JMenuItem newGame = new JMenuItem("New Game");
+        newGame.setMnemonic(KeyEvent.VK_N);
+        
+        JMenuItem exit = new JMenuItem("Exit");
+        exit.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                mainFrame.setVisible(false);
+                mainFrame.dispose();
+            }
+        });
+        
+        exit.setMnemonic(KeyEvent.VK_X);
+        
+        game.add(newGame);
+        game.add(exit);
+        
+        JMenuItem aboutConnect4 = new JMenuItem("About Connect4");
+                
+        aboutConnect4.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                JDialog aboutDialog = new JDialog(mainFrame, "About Connect 4", true);
+                JTextArea aboutText = new JTextArea("Java Again!\n\nGame of Connect 4\nCoded by:\natorrescano@gmail.com\nguadalupe.chilton@gmail.com");
+                aboutDialog.add(aboutText);
+                aboutDialog.pack();
+                aboutDialog.setLocationRelativeTo(mainFrame);
+                aboutDialog.setVisible(true);
+            }
+            });
+        
         mainMenu.add(game);
         mainMenu.add(aboutConnect4);
         
         mainFrame.setJMenuBar(mainMenu);
         mainFrame.pack();        
+        mainFrame.setLocationRelativeTo(null);
         mainFrame.setVisible(true);
     }
 }
