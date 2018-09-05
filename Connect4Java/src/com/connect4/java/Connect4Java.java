a8705df4a49f588875287c72502ecf3aec95bbdc
Aaron Torrescano <atorrescano@gmail.com>
2018-09-04 20:52:53 -0500

    Creating UI package and separating the frame from the main class

--- Connect4Java/src/com/connect4/java/Connect4Java.java
+++ Connect4Java/src/com/connect4/java/Connect4Java.java
@@ -5,6 +5,7 @@
  */
 package com.connect4.java;
 
+import com.connect4.java.ui.Connect4JFrame;
 import com.sun.glass.events.KeyEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -25,59 +26,16 @@ import javax.swing.KeyStroke;
  */
 public class Connect4Java {
 
-    private JMenuBar mainMenu;
+//    private JMenuBar mainMenu;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Connect4Java connect4Java = new Connect4Java();      
+        Connect4JFrame.main(null);
     }
     
     public Connect4Java(){
-        JFrame mainFrame = new JFrame("Connect 4");        
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     
-        
-        mainMenu = new JMenuBar();
-        
-        JMenu game = new JMenu("Game");
-        game.setMnemonic(KeyEvent.VK_G);
-        
-        JMenuItem newGame = new JMenuItem("New Game");
-        newGame.setMnemonic(KeyEvent.VK_N);
-        
-        JMenuItem exit = new JMenuItem("Exit");
-        exit.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                mainFrame.setVisible(false);
-                mainFrame.dispose();
-            }
-        });
-        
-        exit.setMnemonic(KeyEvent.VK_X);
-        
-        game.add(newGame);
-        game.add(exit);
-        
-        JMenuItem aboutConnect4 = new JMenuItem("About Connect4");
-                
-        aboutConnect4.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                JDialog aboutDialog = new JDialog(mainFrame, "About Connect 4", true);
-                JTextArea aboutText = new JTextArea("Java Again!\n\nGame of Connect 4\nCoded by:\natorrescano@gmail.com\nguadalupe.chilton@gmail.com");
-                aboutDialog.add(aboutText);
-                aboutDialog.pack();
-                aboutDialog.setLocationRelativeTo(mainFrame);
-                aboutDialog.setVisible(true);
-            }
-            });
-        
-        mainMenu.add(game);
-        mainMenu.add(aboutConnect4);
-        
-        mainFrame.setJMenuBar(mainMenu);
-        mainFrame.pack();        
-        mainFrame.setLocationRelativeTo(null);
-        mainFrame.setVisible(true);
+        Connect4JFrame connect4 = new Connect4JFrame();
     }
 }
